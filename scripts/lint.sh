7b968f8b577b3c42309122115d81979168019053
AnsibleGuy <guy@ansibleguy.net>
2023-04-02 10:41:24 +0200

    ansible-lint collections-path

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -18,7 +18,11 @@ yamllint .
 echo ''
 echo 'LINTING Ansible'
 echo ''
-ansible-lint -c .ansible-lint.yml
+TMP_COL_DIR='/tmp/ansible_lint/collections'
+mkdir -p "$TMP_COL_DIR/ansible_collections/ansibleguy"
+ln -s "$(pwd)" "$TMP_COL_DIR/ansible_collections/ansibleguy/opnsense"
+ANSIBLE_COLLECTIONS_PATH="$TMP_COL_DIR" ansible-lint -c .ansible-lint.yml
+rm -rf "$TMP_COL_DIR"
 
 echo ''
 echo 'FINISHED LINTING!'
