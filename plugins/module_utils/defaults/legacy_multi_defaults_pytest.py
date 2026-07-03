39d0c1d9e77e91da06c62f24ac2b76f44b0a1bf4
Rath Pascal <rath@superstes.at>
2026-07-03 14:33:47 +0200

    chore: cleanup legacy multi-entry-handling

--- plugins/module_utils/defaults/legacy_multi_defaults_pytest.py
+++
@@ -1,3 +0,0 @@
-def test_placeholder():
-    from ansible_collections.oxlorg.opnsense.plugins.module_utils.defaults.legacy_multi import \
-        PURGE_MOD_ARGS, STATE_MOD_ARG_MULTI, FAIL_MOD_ARG_MULTI, INFO_MOD_ARG, RULE_MOD_ARG_KEY_FIELD
